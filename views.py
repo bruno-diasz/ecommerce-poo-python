0ba4adec97ca10059bf95994cdff99c766391846
Bruno <brunoipb1@gmail.com>
2025-06-05 18:43:43 +0000

    Iniciando a view

---
+++ views.py
@@ -0,0 +1,52 @@
+class View:
+
+    #==== Classe Cliente ====
+    @staticmethod
+    def cliente_listar():
+        pass
+
+    @staticmethod
+    def cliente_inserir():
+        pass
+
+    @staticmethod
+    def cliente_atualizar():
+        pass
+
+    @staticmethod
+    def cliente_excluir():
+        pass
+
+    #===== Classe Produto ======
+    @staticmethod
+    def produto_listar():
+        pass
+
+    @staticmethod
+    def produto_inserir():
+        pass
+
+    @staticmethod
+    def produto_atualizar():
+        pass
+
+    @staticmethod
+    def produto_excluir():
+        pass
+  
+    #===== Classe Categoria ======
+    @staticmethod
+    def categoria_listar():
+        pass
+
+    @staticmethod
+    def categoria_inserir():
+        pass
+
+    @staticmethod
+    def categoria_atualizar():
+        pass
+
+    @staticmethod
+    def categoria_excluir():
+        pass
