bdd70a58b5a389e3ec57ea15249eff187918fdcd
Bruno Diazs <brunoipb1@gmail.com>
2025-06-20 20:58:08 -0300

    alterando formatação do historico de compras

--- templates/historico_venda.py
+++ templates/historico_venda.py
@@ -9,7 +9,7 @@ class HistoricoVenda:
             def __init__(self):
                 pass
             def __str__(self):
-                return f"{'ITEM':<27}     {'PREÇO':>15}   {'QTD':<6} {'SUBTOTAL':>24}"
+                return f"{'ITEM':<27}     {'PREÇO':>15}   {'QTD':<6} {'SUBTOTAL':>25}"
                 
         c = cabecalho()
         st.subheader(":material/receipt: Histórico de Vendas")
@@ -18,7 +18,7 @@ class HistoricoVenda:
         items = View.vendaitem_listar()
         for venda in vendas:
             if venda.carrinho:
-                with st.expander(f'ID: {venda.id} - {venda.data} | TOTAL: R$ {venda.total:.2f}', icon=':material/shopping_cart:'):
+                with st.expander(f'ID: {venda.id} ㅤ {venda.data}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**TOTAL: R$ {venda.total:.2f}**', icon=':material/shopping_cart:'):
                     st.caption(f'CLIENTE: {venda.idCliente}')
                     st.write('---')
                     st.write(c)
@@ -26,7 +26,7 @@ class HistoricoVenda:
                         if item.idVenda == venda.id:
                             st.write(item)
             else:
-                with st.expander(f'ID: {venda.id} - {venda.data} | TOTAL: R$ {venda.total:.2f}', icon=':material/paid:'):
+                with st.expander(f'ID: {venda.id} ㅤ {venda.data}ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**TOTAL: R$ {venda.total:.2f}**', icon=':material/paid:'):
                     st.caption(f'CLIENTE: {venda.idCliente}')
                     st.write('---')
                     st.write(c)
